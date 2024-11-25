b222c021f802ef8a03af5324c1b64404b8e353a2
Dhanaraj73 <dhanarajpammar395@gmail.com>
2024-11-25 16:15:29 +0530

    code.sv

---
+++ orange/a/code.sv
@@ -0,0 +1,9 @@
+module job_q_2_1 (
+    input logic InputA, InputB, InputC, InputD,
+    output logic OutputY
+);
+    assign OutputY = ~InputA | 
+                     (InputA & InputB & InputD) | 
+                     (InputA & InputC & ~InputD) | 
+                     (InputA & ~InputB & ~InputC);
+Endmodule
